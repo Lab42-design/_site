1b94f79f9effc0eb1bed835e04779b71f6bf1f3e
Zsolt Sándor <ezsolt@gmail.com>
2021-12-08 18:45:47 +0100

    ts sources

---
+++ src/index.ts
@@ -0,0 +1,51 @@
+// imports
+import PromiseDom from "./promiseDom.js"
+import Router from "./router.js"
+import FetchPartial from "./fetchPartial.js"
+
+
+
+// check if dom is ready
+let dom: any = new PromiseDom
+dom.ready.then(__start())
+
+function __start(): void {
+    console.log('---------------------')
+    console.log(' dom is ready')
+    console.log('---------------------')
+    console.log(' starting')
+    
+    includePartials()
+
+}
+
+
+
+// router
+const router = new Router({
+    type: "hash",
+    routes: {
+        "/":         "home",
+        "/about":    "about",
+        "/products": "products"
+    }
+}).listen().on("route", async e => {
+    console.log(e.detail.route, e.detail.url)
+    document.querySelector("section").innerHTML = await fetch("/" + e.detail.route + ".html").then(x=>x.text())
+})
+
+// partial
+function includePartials(): void {
+    console.log('---------------------')
+    console.log(' including partials')
+    const partial = new FetchPartial()
+    partial.fetchAll()
+}
+
+
+
+
+
+
+
+
