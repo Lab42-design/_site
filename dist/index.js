924fd7950639386754e3b32543c81e550cd7bda5
Zsolt Sándor <ezsolt@gmail.com>
2021-12-08 18:46:36 +0100

    dist

---
+++ dist/index.js
@@ -0,0 +1,33 @@
+// imports
+import PromiseDom from "./promiseDom.js";
+import Router from "./router.js";
+import FetchPartial from "./fetchPartial.js";
+// check if dom is ready
+let dom = new PromiseDom;
+dom.ready.then(__start());
+function __start() {
+    console.log('---------------------');
+    console.log(' dom is ready');
+    console.log('---------------------');
+    console.log(' starting');
+    includePartials();
+}
+// router
+const router = new Router({
+    type: "history",
+    routes: {
+        "/": "home",
+        "/about": "about",
+        "/products": "products"
+    }
+}).listen().on("route", async (e) => {
+    console.log(e.detail.route, e.detail.url);
+    document.querySelector("section").innerHTML = await fetch("/" + e.detail.route + ".html").then(x => x.text());
+});
+// partial
+function includePartials() {
+    console.log('---------------------');
+    console.log(' including partials');
+    const partial = new FetchPartial();
+    partial.fetchAll();
+}
